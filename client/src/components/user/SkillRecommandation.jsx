74ffae17c9f4e10cb82a16bca612cb56dfa58a6d
SHUBHAM2775 <shubhamu1332@gmail.com>
2025-10-09 02:28:13 +0530

    recommend-fix

--- client/src/components/user/SkillRecommandation.jsx
+++ client/src/components/user/SkillRecommandation.jsx
@@ -231,7 +231,6 @@ const SkillRecommendations = () => {
             </div>
           </div>
         </div>
-      </div>
 
       {/* Main Content - Grid View */}
       <div className="max-w-7xl mx-auto px-4 py-6">
@@ -299,6 +298,7 @@ const SkillRecommendations = () => {
           </div>
         )}
       </div>
+      </main>
     </div>
   );
 };
